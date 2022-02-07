d70ae6437de7e9c59408e108584ffbfdba978de7
Baziibro <96934642+hyko-xd@users.noreply.github.com>
2022-02-07 11:29:17 +0530

    Update punishment.py

--- NoinoiMusic/PLUGINS/punishment.py
+++ NoinoiMusic/PLUGINS/punishment.py
@@ -5,12 +5,12 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
-from driver.filters import command
-from driver.decorators import sudo_users_only
-from driver.database.dbchat import get_served_chats
-from driver.database.dbpunish import add_gban_user, is_gbanned_user, remove_gban_user
+from NoinoiMusic.DREAMS.filters import command
+from NoinoiMusic.DREAMS.decorators import sudo_users_only
+from NoinoiMusic.DREAMS.database.dbchat import get_served_chats
+from NoinoiMusic.DREAMS.database.dbpunish import add_gban_user, is_gbanned_user, remove_gban_user
 
-from config import BOT_NAME, SUDO_USERS, BOT_USERNAME as bn
+from NoinoiMusic.config import BOT_NAME, SUDO_USERS, BOT_USERNAME as bn
 
 
 @Client.on_message(command(["gban", f"gban@{bn}"]) & ~filters.edited)
